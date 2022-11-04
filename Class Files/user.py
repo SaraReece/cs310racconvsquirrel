ef493813eb8080849f0b93908bf6d0a4fd2aea2e
Eli Cutchen <65992004+ejctchn@users.noreply.github.com>
2022-11-03 23:38:17 -0600

    Updated user.py to append the list

--- Class Files/user.py
+++ Class Files/user.py
@@ -1,21 +1,26 @@
 class user():
     def __init__(self, userName):
         import json
+        filename = "Class Files/test.json"
         self.userName = userName
         self.userScore = 0
         self.combined = userName + " Score: " + str(self.userScore)
 
         #CreateUserName
-        json_object = json.dumps(self.combined, indent=4)
+        #json_object = json.dumps(self.combined)
+        entry = {'user': userName, 'score': self.userScore}
 
-        with open("Class Files/test.json", "w") as outfile:
-            outfile.write(json_object)
+        with open(filename, "r") as file:
+            file_data = json.load(file)
+            file_data.append(entry)
+        with open(filename, "w") as file:
+            json.dump(file_data, file)
 
     def updateUserData(self, score):
         self.userScore = score
 
 #Debugging and testing
-name = "Joseph"
+name = "Marissa"
 newuser = user(name)
 newuser.updateUserData(100)
 
