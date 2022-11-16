848adf7d8e7e177637dd4b2a5e01afeff8dd5f00
Eli Cutchen <65992004+ejctchn@users.noreply.github.com>
2022-11-16 15:52:47 -0700

    minor changes to code

--- Class Files/user.py
+++ Class Files/user.py
@@ -1,28 +1,50 @@
 class user():
     def __init__(self, userName):
-        import json
-        filename = "Class Files/test.json"
         self.userName = userName
         self.userScore = 0
-        self.combined = userName + " Score: " + str(self.userScore)
 
-        #CreateUserName
-        #json_object = json.dumps(self.combined)
-        entry = {'user': userName, 'score': self.userScore}
+    def createUserName(self, userName):
+        import json
+        filename = "Class Files/test.json"
 
+        entry = {'user': userName}, {'score': self.userScore}
+
+        #open the file and read it
         with open(filename, "r") as file:
             file_data = json.load(file)
             file_data.append(entry)
+
+        #write the new info to the file
         with open(filename, "w") as file:
             json.dump(file_data, file)
 
-    def updateUserData(self, score):
+    #update the score to an existing user
+    def updateUserData(self, score, name):
+        import json
+        filename = "Class Files/test.json"
+
         self.userScore = score
+
+        entry = {'score': score}
+        #open the file and read it
+        with open(filename, "r") as file:
+            file_data = json.load(file)
+            for x in file_data:
+                print(x)
+                if x == "{'user': '" + name + "'}":
+                    file_data[x + 1] = entry
+                else:
+                    print("nope")
+
+        #write the new info to the file
+        with open(filename, "w") as file:
+            json.dump(file_data, file)
+
 
 #Debugging and testing
 name = "Marissa"
-newuser = user(name)
-newuser.updateUserData(100)
+#newuser = user(name)
+user(name).updateUserData(100, name)
 
-print(f"Hello {newuser.userName}")
-print(f"Your score is: {newuser.userScore}")
+#print(f"Hello {newuser.userName}")
+#print(f"Your score is: {newuser.userScore}")
