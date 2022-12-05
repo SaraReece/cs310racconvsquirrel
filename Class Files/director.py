713ee3800d49905647a7742149615cc49ac15902
Bryce Sanders <104106284+bryce-sanders@users.noreply.github.com>
2022-12-05 14:32:37 -0800

    Creating Title Screen

--- Class Files/director.py
+++ Class Files/director.py
@@ -20,6 +20,13 @@ class Director():
             visuals: The list of Visual objects that the Director
                 should be displaying.
             clock: A PyGame Clock object used to regulate framerate.
+            background_image_location: The file path to the location
+                of the background image.
+            background: The background image loaded into a useable
+                PyGame 'Image' object.
+            file_selected: A Boolean variable to track if the player
+                has selected a file or not.
+            
         """
         self.screen_width = 412
         self.screen_height = 732
@@ -28,6 +35,7 @@ class Director():
         self.clock = pygame.time.Clock()
         self.background_image_location = "background.png"
         self.background = pygame.image.load(self.background_image_location)
+        self.file_selected = False
 
 
     def main(self):
@@ -40,7 +48,10 @@ class Director():
 
         # Create example objects.
         # TODO: Remove from final product.
-        self.create_test_objects()
+        # self.create_test_objects()
+
+        # Create the title at the top of the game.
+        self.save_file_select_screen()
 
         # The gameplay loop. Loops until the player closes
         # the window.
@@ -70,6 +81,28 @@ class Director():
 
             # Run the "game" at 60 fps.
             self.clock.tick(60)
+
+
+    def save_file_select_screen(self):
+        """
+        Displays the first screen of the game to the player. This
+        allows the player to select one of the three player files
+        to use. This will either be an existing user, or a new
+        'Empty' file.
+        """
+        # Create a "Visual" object to display the game's title at
+        # the top of the screen.
+        title_visual = Visual("Raccoon VS Squirrel", 300, 100, 56, 28, (184,180,156))
+        self.visuals.append(title_visual)
+
+        # Create "Visual" objects to display the game's save files
+        # on screen.
+        save_file_1 = Visual("Empty", 300, 100, 56, 350, (184,72,120), True)
+        self.visuals.append(save_file_1)
+        save_file_2 = Visual("Empty", 300, 100, 56, 475, (184,72,120), True)
+        self.visuals.append(save_file_2)
+        save_file_3 = Visual("Empty", 300, 100, 56, 600, (184,72,120), True)
+        self.visuals.append(save_file_3)
 
 
     def create_test_objects(self):
