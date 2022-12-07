8212fad4262ccb9aea19a3816119a922451de86a
Bryce Sanders <104106284+bryce-sanders@users.noreply.github.com>
2022-12-07 13:39:43 -0800

    Working Save File Select Screen

--- Class Files/director.py
+++ Class Files/director.py
@@ -1,7 +1,7 @@
 import pygame
 from visual import Visual
 from sys import exit
-import os
+import json
 
 # Start the PyGame module.
 pygame.init()
@@ -26,6 +26,7 @@ class Director():
                 PyGame 'Image' object.
             file_selected: A Boolean variable to track if the player
                 has selected a file or not.
+<<<<<<< Updated upstream
             current_player: A variable to hold the current player's number.
                 If the player has not been selected yet, this string is
                 empty.
@@ -34,6 +35,17 @@ class Director():
                 number is 0.
             
             
+=======
+            save_file_path: The file path to the json file containing
+                player data.
+            users: A Python dictionary obtained from 'save_files.json'.
+            player_selected: A Boolean variable to track if a player has
+                been selected.
+            current_user: A variable to keep track of the current user.
+            in_game: A Boolean variable to track if a continent has
+                been selected.
+            current_continent: The currently selected continent.
+>>>>>>> Stashed changes
         """
         self.screen_width = 412
         self.screen_height = 732
@@ -42,8 +54,12 @@ class Director():
         self.clock = pygame.time.Clock()
         self.background_image_location = "background.png"
         self.background = pygame.image.load(self.background_image_location)
-        self.current_player_number = 0
-        self.current_player_score = 0
+        self.save_file_path = "Class Files/save_files.json"
+        self.users = self.load_users()
+        self.player_selected = False
+        self.current_user = ""
+        self.in_game = False
+        self.current_continent = ""
 
 
     def main(self):
@@ -79,16 +95,33 @@ class Director():
             for i in self.visuals:
                 i.display(window)
 
-            # Check to see if the visuals in the window
-            # need to do anything.
-            for i in self.visuals:
-                i.do_stuff()
+            # While the user has not selected a save file...
+            if not self.player_selected:
+                # Check to see if the save file select buttons
+                # in the window need to do anything.
+                for i in self.visuals:
+                    self.player_selected = i.player_select_button()
+                    # If one has been selected, get rid of the save
+                    # file select buttons on screen.
+                    if self.player_selected:
+                        self.destroy_save_file_select_screen(i)
+            # Otherwise the user has selected a save file.
+            else:
+                pass
 
             # Update the window.
             pygame.display.update()
 
             # Run the "game" at 60 fps.
             self.clock.tick(60)
+
+
+    def load_users(self):
+        # Open the file and read it.
+        with open(self.save_file_path, "r") as file:
+            users = json.load(file)
+        # Return file data as Python dictionary.
+        return users
 
 
     def save_file_select_screen(self):
@@ -100,17 +133,34 @@ class Director():
         """
         # Create a "Visual" object to display the game's title at
         # the top of the screen.
-        title_visual = Visual("Raccoon VS Squirrel", 300, 100, 56, 28, (184,180,156))
+        title_visual = Visual("Title", "Raccoon VS Squirrel", 300, 100, 56, 28, (184,180,156))
         self.visuals.append(title_visual)
 
         # Create "Visual" objects to display the game's save files
-        # on screen.
-        save_file_1 = Visual(f"Player 1:  Pts", 300, 100, 56, 350, (184,72,120), True)
-        self.visuals.append(save_file_1)
-        save_file_2 = Visual(f"Player 2:  Pts", 300, 100, 56, 475, (184,72,120), True)
-        self.visuals.append(save_file_2)
-        save_file_3 = Visual(f"Player 3:  Pts", 300, 100, 56, 600, (184,72,120), True)
-        self.visuals.append(save_file_3)
+        # on screen. Get user scores from self.users.
+        userOneScore = self.users[0]["userOne"]
+        save_file_one = Visual("userOne", f"Player 1: {userOneScore} Pts", 300, 100, 56, 350, (184,72,120), True)
+        self.visuals.append(save_file_one)
+        userTwoScore = self.users[1]["userTwo"]
+        save_file_two = Visual("userTwo", f"Player 2: {userTwoScore} Pts", 300, 100, 56, 475, (184,72,120), True)
+        self.visuals.append(save_file_two)
+        userThreeScore = self.users[2]["userThree"]
+        save_file_three = Visual("userThree", f"Player 3: {userThreeScore} Pts", 300, 100, 56, 600, (184,72,120), True)
+        self.visuals.append(save_file_three)
+
+
+    def destroy_save_file_select_screen(self, i):
+        """
+        Gets rid of the buttons created by save_file_select_screen().
+        """
+        # Set self.current_user based on the user's selection.
+        self.current_user = i.name
+        # Log which player wax selected in the terminal.
+        print(f"{self.current_user} was selected")
+        # Delete everything but the title Visual in self.visuals.
+        title = self.visuals[0]
+        self.visuals.clear()
+        self.visuals.append(title)
 
 
     def create_test_objects(self):
---
+++ Class Files/user.py
@@ -0,0 +1,111 @@
+class user():
+    def __init__(self):
+        self.userScore = 0
+
+    def createUserName(self):
+        import json
+        filename = "Class Files/test.json"
+        userName = input("Please enter your name: ")
+
+        entry = []
+        
+        entry.append({'name': userName, 'score': self.userScore})
+
+        #open the file and read it
+        with open(filename, "r") as file:
+            file_data = json.load(file)
+            file_data.append(entry)
+
+        #write the new info to the file
+        with open(filename, "w") as file:
+            json.dump(file_data, file)
+
+    #update the score to an existing user
+    def updateUserData(self, score, name):
+        import json
+        filename = "Class Files/test.json"
+        counter = 1
+
+        self.userScore = score
+
+        entry = {'name': name, 'score': score}
+
+        #open the file and read it
+        with open(filename, "r") as file:
+            file_data = json.load(file)
+            
+            print("Thanks for playing!")
+            for x in file_data:
+                print(f"{counter}. {x}")
+                counter += 1
+
+            #select your name    
+            number = int(input("Please choose save destination(1-9): "))
+            file_data[number - 1] = entry
+            
+        #write the new info to the file
+        with open(filename, "w") as file:
+            json.dump(file_data, file)
+
+
+    def loadUserName(self):
+        import json
+        filename = "Class Files/test.json"
+        counter = 1
+        nameCheck = False
+        scoreCheck = False
+        name = ""
+        score = ""
+
+        with open(filename, "r") as file:
+            file_data = json.load(file)
+
+        for x in file_data:
+            print("Saved Users: ")
+            print(f"{counter}. {x}")
+            counter += 1
+
+        load = int(input("Please choose your file save: "))
+        strData = str(file_data[load - 1])
+        i = 0
+        count = 0
+        for y in strData:
+            if strData[i] == ":" and strData[i - 3] == "m":
+                nameCheck = True
+                count = i + 3
+                i += 1
+                while nameCheck:
+                    for z in strData:
+                        if strData[count] == "'":
+                            nameCheck = False
+                        else:
+                            name += strData[count]
+                            count += 1
+            elif strData[i] == ":" and strData[i - 3] == "r":
+                scoreCheck = True
+                count = i + 2
+
+                while scoreCheck:
+                    for z in strData:
+                        if strData[count] == "}":
+                            scoreCheck = False
+                        else:
+                            score += (strData[count])
+                            count += 1
+                i += 1
+                score = int(score)
+            else:
+                i += 1
+        
+        finalString = (f"Name: {name}, Score: {score}")
+
+        return finalString
+# #Debugging and testing
+
+# #name = "Jason"
+# newuser = user()
+# #newuser.createUserName()
+# newuser.updateUserData(500, "Eli")
+# print("Successfully updated!")
+# #loaded = newuser.loadUserName()
+# #print(f"Successfully loaded -> {loaded}")
